1ffec2562bdeeb5f64b0f8b32d6da0d2b41194b7
Belmin Zoletić <belminzola@gmail.com>
2025-11-28 15:54:49 +0100

    - Created complete desing for Master Page - CSS for master page DONE - Added icons for elements on side bar

--- inventory-management-system/Site1.Master.designer.cs
+++ inventory-management-system/Site1.Master.designer.cs
@@ -85,5 +85,23 @@ namespace inventory_management_system
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink HyperLinkContact;
+
+        /// <summary>
+        /// TextBoxSearch control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxSearch;
+
+        /// <summary>
+        /// ButtonSearch control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonSearch;
     }
 }
